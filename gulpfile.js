d8a6d099cf3c65457bdd4cd64bc45e3c71f46f45
Evgeniya Malyushkina <e.malyushkina@mail.ru>
2020-08-30 12:58:08 +0800

    Исправила ошибку с модальным окном, вернула сборку

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ const styles = () => {
     .pipe(csso())
     .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 };
 
@@ -132,57 +132,6 @@ const html = () => {
 };
 exports.html = html;
 
-// const build = gulp.series(clean, copy, styles, images, toWebp, sprite, html);
-const build = gulp.series(clean, copy, styles, html);
+const build = gulp.series(clean, copy, styles, images, toWebp, sprite, html);
+// const build = gulp.series(clean, copy, styles, html);
 exports.build = build;
-// const gulp = require("gulp");
-// const plumber = require("gulp-plumber");
-// const sourcemap = require("gulp-sourcemaps");
-// const less = require("gulp-less");
-// const postcss = require("gulp-postcss");
-// const autoprefixer = require("autoprefixer");
-// const sync = require("browser-sync").create();
-
-// // Styles
-
-// const styles = () => {
-//   return gulp.src("source/less/style.less")
-//     .pipe(plumber())
-//     .pipe(sourcemap.init())
-//     .pipe(less())
-//     .pipe(postcss([
-//       autoprefixer()
-//     ]))
-//     .pipe(sourcemap.write("."))
-//     .pipe(gulp.dest("source/css"))
-//     .pipe(sync.stream());
-// }
-
-// exports.styles = styles;
-
-// Server
-
-// const server = (done) => {
-//   sync.init({
-//     server: {
-//       baseDir: 'source'
-//     },
-//     cors: true,
-//     notify: false,
-//     ui: false,
-//   });
-//   done();
-// }
-
-// exports.server = server;
-
-// // Watcher
-
-// const watcher = () => {
-//   gulp.watch("source/less/**/*.less", gulp.series("styles"));
-//   gulp.watch("source/*.html").on("change", sync.reload);
-// }
-
-// exports.default = gulp.series(
-//   styles, server, watcher
-// );
